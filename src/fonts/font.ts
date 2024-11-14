e4d2fd82e1a765ce03cfea3b4a98cdf1c2cb4e7e
patricklimax <patricklimax@outlook.com.br>
2024-11-13 21:47:30 -0300

    chore: setup initial, Biome, Prettier, Fonts

---
+++ src/fonts/font.ts
@@ -0,0 +1,12 @@
+import { JetBrains_Mono, Space_Grotesk, Squada_One } from "next/font/google";
+
+export const spaceGrotesk = Space_Grotesk({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const squadaOne = Squada_One({ weight: ["400"], subsets: ["latin"] });
+export const jetBrainsMono = JetBrains_Mono({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
