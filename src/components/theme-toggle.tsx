16a72e306d7896538f86b89f645e95391fb9a534
patricklimax <patricklimax@outlook.com.br>
2024-11-14 23:22:29 -0300

    refactor: update Component Theme Toggle

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,34 +1,46 @@
 'use client';
+
 import { Button } from '@/components/ui/button';
-import { MoonIcon, SunIcon } from 'lucide-react';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-import React, { useState, useEffect } from 'react';
 
-export const ToggleTheme = () => {
-	const { theme, setTheme } = useTheme();
-	const [isClient, setIsClient] = useState(false);
-
-	useEffect(() => {
-		setIsClient(true);
-	}, []);
-
-	if (!isClient) return null;
-
-	const handleToggleTheme = () => {
-		setTheme(theme === 'dark' ? 'light' : 'dark');
-	};
+export function ToggleTheme() {
+	const { setTheme } = useTheme();
 
 	return (
-		<Button
-			size={'icon'}
-			variant={'outline'}
-			type='button'
-			onClick={handleToggleTheme}>
-			{theme === 'dark' ? (
-				<SunIcon strokeWidth={2} />
-			) : (
-				<MoonIcon strokeWidth={2} />
-			)}
-		</Button>
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant='outline'
+					size='icon'
+					className='border text-primary h-9 w-9 focus:outline-none focus:ring-0'>
+					<Sun
+						size={20}
+						className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'
+					/>
+					<Moon
+						size={20}
+						className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100'
+					/>
+					<span className='sr-only'>Toggle theme</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuItem onClick={() => setTheme('light')}>
+					<Sun className='h-[1.2rem] w-[1.2rem]' />
+					{/* Light */}
+				</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme('dark')}>
+					<Moon className='h-[1.2rem] w-[1.2rem]' />
+					{/* Dark */}
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
-};
+}
