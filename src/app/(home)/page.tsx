4741d913935858e9d1510772b8ccddb160a191ab
patricklimax <patricklimax@outlook.com.br>
2024-11-19 00:13:48 -0300

    feat: created Component Home Page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,19 @@
+import { SectionPage } from '@/components/section';
+
 export default function Home() {
 	return (
-		<div>
-			<div className='h-40'>Página Home Page</div>
-			<div className='h-40'>Página Home Page</div>
-			<div className='h-40'>Página Home Page</div>
-			<div className='h-40'>Página Home Page</div>
-			<div className='h-40'>Página Home Page</div>
-			<div className='h-40'>Página Home Page</div>
-		</div>
+		<SectionPage>
+			<div className='mx-auto grid grid-cols-1 gap-6 sm:h-[calc(100vh-3rem)] md:grid-cols-12 md:grid-rows-12 pb-10 sm:pb-0'>
+				<div className='md:col-start-1 md:col-end-4 md:row-start-1 md:row-end-10'></div>
+
+				<div className='md:col-start-4 md:col-end-10 md:row-start-1 md:row-end-10'></div>
+
+				<div className='md:col-start-10 md:col-end-13 md:row-start-1 md:row-end-10 flex flex-col gap-4 overflow-y-auto [&::-webkit-scrollbar]:hidden'></div>
+
+				<div className='md:col-start-1 md:col-end-7 md:row-start-10 md:row-end-13'></div>
+
+				<div className='md:col-start-7 md:col-end-13 md:row-start-10 md:row-end-13'></div>
+			</div>
+		</SectionPage>
 	);
 }
